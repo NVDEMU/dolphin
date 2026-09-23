a484456ce2d9ab4770210da3c15decb7e0b2bf9f
NVDEMU <ndelpo318@gmail.com>
2026-09-23 13:50:31 -0400

    nvwii: use supplied logo for application icon

--- Source/Core/DolphinQt/Resources.cpp
+++ Source/Core/DolphinQt/Resources.cpp
@@ -95,7 +95,7 @@ void Resources::Init()
   }
 
   m_misc.append(GetResourceIcon("nobanner"));
-  m_misc.append(GetResourceIcon("dolphin_logo"));
+  m_misc.append(GetResourceIcon("nvwii_logo"));
 }
 
 QIcon Resources::GetPlatform(DiscIO::Platform platform)
